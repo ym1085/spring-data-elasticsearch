b940528407c03adf833a72677239f2e623a6e32b
ym1085 <youngmin1085@gmail.com>
2022-02-24 01:10:22 +0900

    CHORE: ElasticsearchOperations bean 추가

--- src/main/java/com/elasticsearch/config/RestClientConfig.java
+++ src/main/java/com/elasticsearch/config/RestClientConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
 
 @Configuration
 public class RestClientConfig extends AbstractElasticsearchConfiguration {
@@ -15,12 +18,15 @@ public class RestClientConfig extends AbstractElasticsearchConfiguration {
     @Override
     @Bean
     public RestHighLevelClient elasticsearchClient() {
-        final ClientConfiguration clientConfig =
-                ClientConfiguration.builder()
+        final ClientConfiguration clientConfig
+                = ClientConfiguration.builder()
                         .connectedTo(ELASTICSEARCH_HOST)
                         .build();
         return RestClients.create(clientConfig).rest();
     }
 
-
+    @Bean
+    public ElasticsearchOperations elasticsearchOperations() {
+        return new ElasticsearchRestTemplate(elasticsearchClient());
+    }
 }
